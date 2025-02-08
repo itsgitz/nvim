051bb1d9e94a19919dde48a69ca655604f28edcf
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-02-08 10:43:45 +0700

    fix: line number colors and unused variables color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,5 +6,9 @@ return {
       sidebars = "transparent",
       floats = "transparent",
     },
+    on_highlights = function(highlights, colors)
+      highlights.DiagnosticUnnecessary.fg = "#709fba"
+      highlights.LineNr.fg = "#BD93F9"
+    end,
   },
 }
