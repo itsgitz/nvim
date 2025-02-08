0ec242c494b442f8e65b1e8fa302db16d46def14
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-02-08 09:59:44 +0700

    chore: make nvim transparent background

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -51,3 +51,11 @@ require("lazy").setup({
     },
   },
 })
+
+-- function Transparent(color)
+--   color = color or "tokyonight"
+--   vim.cmd.colorscheme(color)
+--   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+-- Transparent()
