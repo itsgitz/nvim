f2b89ef63a3b112cec38be8ee3dd1369e1cea431
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-02-09 10:17:36 +0700

    chore: updated cursor line number color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,7 +8,8 @@ return {
     },
     on_highlights = function(highlights, colors)
       highlights.DiagnosticUnnecessary.fg = "#709fba"
-      highlights.LineNr.fg = "#BD93F9"
+      highlights.LineNr.fg = "#669afa"
+      highlights.CursorLineNr.fg = "#ffffff"
     end,
   },
 }
