e46223453542be7b25269d870a207bca2258df5f
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-03-04 19:16:15 +0700

    chore: rollback to default typscript lsp

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,15 +1,7 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      -- Disable VTSLS by overriding its setup function
-      lspconfig.vtsls.setup = function() end -- This effectively disables the VTSLS
-    end,
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
-}
+-- return {
+--   {
+--     "pmizio/typescript-tools.nvim",
+--     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+--     opts = {},
+--   },
+-- }
