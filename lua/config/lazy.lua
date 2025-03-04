3e7b0265bd77a188b3c3b9b627f280f8ec576cd6
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-03-04 19:08:00 +0700

    fix: disabled vtsls

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -59,3 +59,4 @@ require("lazy").setup({
 --   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- end
 -- Transparent()
+--
--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,5 +1,15 @@
 return {
-  "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      -- Disable VTSLS by overriding its setup function
+      lspconfig.vtsls.setup = function() end -- This effectively disables the VTSLS
+    end,
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
 }
