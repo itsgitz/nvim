a424ae0bf1e72c5e2f0150e9c073a212fbb8c7f0
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-04-01 09:10:36 +0700

    feat: added custom lsp config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "css-lsp",
+      })
+    end,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+    end,
+  },
+}
