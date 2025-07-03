381754f6a0575132e294d92b96e699abed1d2566
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-07-03 14:19:05 +0700

    fix: php formatter configuration

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -1,12 +1,35 @@
 return {
   {
     "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        php = { { "pint", "php_cs_fixer" } },
-      },
-    },
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+          php = { "php" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        },
+        notify_on_error = true,
+        formatters = {
+          php = {
+            command = "php-cs-fixer",
+            args = {
+              "fix",
+              "$FILENAME",
+              "--config=/your/path/to/config/file/[filename].php",
+              "--allow-risky=yes", -- if you have risky stuff in config, if not you dont need it.
+            },
+            stdin = false,
+          },
+        },
+      })
+    end,
   },
   {
     -- Remove phpcs linter.
