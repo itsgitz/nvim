ef9ea01781ed639d600390cf2ae5ff5423840014
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-07-08 15:04:50 +0700

    fix: temporarily disabled emmet

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,26 +1,24 @@
-return {
-  "mattn/emmet-vim",
-  ft = {
-    "html",
-    "css",
-    "javascript",
-    "javascriptreact",
-    "typescriptreact",
-    "vue",
-    "php",
-    "blade",
-    "svelte",
-    "smarty",
-    "tpl",
-  },
-  init = function()
-    vim.g.user_emmet_leader_key = "<C-e>" -- Change this if you want a different trigger
-    vim.g.user_emmet_settings = {
-      html = {
-        snippets = {
-          ["html:5"] = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body></body></html>',
-        },
-      },
-    }
-  end,
-}
+-- return {
+--   "mattn/emmet-vim",
+--   ft = {
+--     "html",
+--     "css",
+--     "javascript",
+--     "javascriptreact",
+--     "typescriptreact",
+--     "vue",
+--     "php",
+--     "blade",
+--     "svelte",
+--     "smarty",
+--     "tpl",
+--   },
+--   config = function()
+--     -- Emmet keybinding
+--     vim.api.nvim_set_keymap("i", "<C-e>", "<C-y>,", { noremap = true, silent = true })
+--
+--     -- Optional emmet config
+--     vim.g.user_emmet_leader_key = "<C-e>"
+--     vim.g.user_emmet_settings = {}
+--   end,
+-- }
