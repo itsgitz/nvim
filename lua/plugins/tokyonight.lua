01450e88cf946b8c2bcb94d749b4ab2ef16bc767
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-07-31 06:55:00 +0700

    fix: disabled relative line number config

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -6,5 +6,9 @@ return {
       sidebars = "transparent",
       floats = "transparent",
     },
+    on_highlights = function(hl, c)
+      hl.LineNr = { fg = "#7aa2f7" }
+      hl.CursorLineNr = { fg = "#e0af68", bold = true }
+    end,
   },
 }
