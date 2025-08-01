5695fcc2c5c72eacdcb7bac9107d42333d8bcdb9
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-08-02 05:18:37 +0700

    chore: updated library

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.env.CC = "zig cc"
 vim.opt.relativenumber = false
 vim.opt.mouse = ""
 vim.g.lazyvim_php_lsp = "intelephense"
